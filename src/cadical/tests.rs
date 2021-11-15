bb73d51782ac668df2a97c8cd4ac76442276a328
Konstantin Chukharev <lipen00@gmail.com>
2021-11-15 13:23:31 +0300

    Use Lit::bool, add tests

--- src/cadical/tests.rs
+++ src/cadical/tests.rs
@@ -1,18 +1,11 @@
 use std::convert::TryInto;
 
+use less::CadicalSolver2;
 use solver::CadicalSolver;
 
-use crate::ipasir::{LitValue, SolveResponse};
+use crate::ipasir::SolveResponse;
 
 use super::*;
-
-fn lit_value_to_bool(value: LitValue) -> bool {
-    match value {
-        LitValue::True => true,
-        LitValue::False => false,
-        LitValue::DontCare => panic!("DontCare!"),
-    }
-}
 
 #[test]
 fn test_cadical_solver() -> color_eyre::Result<()> {
@@ -20,10 +13,10 @@ fn test_cadical_solver() -> color_eyre::Result<()> {
     assert!(solver.signature().starts_with("cadical"));
 
     // Adding [(1 or 2) and (3 or 4) and not(1 and 2) and not(3 and 4)]
-    solver.try_add_clause(&[1, 2])?;
+    solver.try_add_clause([1, 2])?;
     solver.try_add_clause(&[3, 4])?;
-    solver.try_add_clause(&[-1, -2])?;
-    solver.try_add_clause(&[-3, -4])?;
+    solver.try_add_clause(vec![-1, -2])?;
+    solver.try_add_clause(&vec![-3, -4])?;
 
     // Problem is satisfiable
     let response = solver.solve()?;
@@ -40,13 +33,50 @@ fn test_cadical_solver() -> color_eyre::Result<()> {
     let response = solver.solve()?;
     assert_eq!(response, SolveResponse::Sat);
 
-    let val1 = lit_value_to_bool(solver.val(1.try_into()?)?);
-    let val2 = lit_value_to_bool(solver.val(2.try_into()?)?);
-    let val3 = lit_value_to_bool(solver.val(3.try_into()?)?);
-    let val4 = lit_value_to_bool(solver.val(4.try_into()?)?);
+    let val1 = solver.val(1.try_into()?)?.bool();
+    let val2 = solver.val(2.try_into()?)?.bool();
+    let val3 = solver.val(3.try_into()?)?.bool();
+    let val4 = solver.val(4.try_into()?)?.bool();
     eprintln!("values: {:?}", vec![val1, val2, val3, val4]);
     assert!(val1 ^ val2);
     assert!(val3 ^ val4);
+
+    Ok(())
+}
+
+#[test]
+fn test_cadical_solver2() -> color_eyre::Result<()> {
+    let solver = CadicalSolver2::new();
+    assert!(solver.signature().starts_with("cadical"));
+
+    // Adding [(1 or 2) and (3 or 4) and not(1 and 2) and not(3 and 4)]
+    solver.add_clause([1, 2]);
+    solver.add_clause(*&[3, 4]);
+    solver.add_clause(vec![-1, -2]);
+    solver.add_clause([-3, -4]);
+
+    // Problem is satisfiable
+    let response = solver.solve();
+    assert_eq!(response, 10); // 10 is SAT, 20 is UNSAT
+
+    // Assuming both 1 and 2 to be true
+    solver.assume(1);
+    solver.assume(2);
+    // Problem is unsatisfiable under assumptions
+    let response = solver.solve();
+    assert_eq!(response, 20); // 10 is SAT, 20 is UNSAT
+
+    // `solve` resets assumptions, so calling it again should produce SAT
+    let response = solver.solve();
+    assert_eq!(response, 10); // 10 is SAT, 20 is UNSAT
+
+    let val1 = solver.val(1);
+    let val2 = solver.val(2);
+    let val3 = solver.val(3);
+    let val4 = solver.val(4);
+    eprintln!("values: {:?}", vec![val1, val2, val3, val4]);
+    assert!((val1 > 0) ^ (val2 > 0));
+    assert!((val3 > 0) ^ (val4 > 0));
 
     Ok(())
 }
