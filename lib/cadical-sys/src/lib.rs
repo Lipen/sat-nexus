92328f320c7905a27fc74ba2bc98a80186555c11
Konstantin Chukharev <lipen00@gmail.com>
2022-02-08 23:34:20 +0300

    Move bindings module to a separate file

--- lib/cadical-sys/src/lib.rs
+++ lib/cadical-sys/src/lib.rs
@@ -1,14 +1,4 @@
-pub mod bindings {
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
-    #![allow(dead_code)]
-    #![allow(deref_nullptr)] // see https://github.com/rust-lang/rust-bindgen/issues/1651
-    #![allow(clippy::style)]
-
-    include!(concat!(env!("OUT_DIR"), "/bindings-ccadical.rs"));
-    // include!("../_bindings-ccadical.rs");
-}
+pub mod bindings;
 
 pub type CCadicalFFI = bindings::ccadical;
 pub type CCadicalPtr = *mut bindings::CCaDiCaL;
