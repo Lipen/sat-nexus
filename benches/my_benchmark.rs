5d414542132b8ab8756d270a910afeb6dcdb9da5
Konstantin Chukharev <lipen00@gmail.com>
2022-02-12 22:50:42 +0300

    Add benches

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,39 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use cadical::Cadical;
+use cadical_sys::statik;
+use minisat::MiniSat;
+
+fn ms_solve(minisat: &MiniSat) {
+    minisat.solve();
+}
+
+fn cadical_solve(cadical: &Cadical) {
+    cadical.solve();
+}
+
+fn ccadical_solve(ptr: *mut statik::CCaDiCaL) {
+    unsafe {
+        statik::ccadical_solve(ptr);
+    }
+}
+
+fn my_benches(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Solvers");
+
+    let minisat = MiniSat::new();
+    group.bench_with_input("MiniSat solve", &minisat, |b, minisat| b.iter(|| ms_solve(minisat)));
+
+    let cadical = Cadical::new();
+    group.bench_with_input("Cadical solve", &cadical, |b, cadical| {
+        b.iter(|| cadical_solve(cadical))
+    });
+
+    let ptr = unsafe { statik::ccadical_init() };
+    group.bench_with_input("CCaDiCaL solve", &ptr, |b, &ptr| b.iter(|| ccadical_solve(ptr)));
+
+    group.finish();
+}
+
+criterion_group!(benches, my_benches);
+criterion_main!(benches);
