98640348f2fcb1835c0321e9664cc44ab380ba49
Konstantin Chukharev <lipen00@gmail.com>
2022-02-15 23:05:34 +0300

    Use glob to get cpp sources. Allowlist only ccadical_* functions

--- lib/cadical-sys/build.rs
+++ lib/cadical-sys/build.rs
@@ -14,81 +14,14 @@ fn main() {
 
 fn build_static_lib() {
     println!("cargo:warning=Building Cadical static library...");
-    let files = [
-        "analyze.cpp",
-        "arena.cpp",
-        "assume.cpp",
-        "averages.cpp",
-        "backtrack.cpp",
-        "backward.cpp",
-        "bins.cpp",
-        "block.cpp",
-        "ccadical.cpp",
-        "checker.cpp",
-        "clause.cpp",
-        "collect.cpp",
-        "compact.cpp",
-        "condition.cpp",
-        "config.cpp",
-        "constrain.cpp",
-        "contract.cpp",
-        "cover.cpp",
-        "decide.cpp",
-        "decompose.cpp",
-        "deduplicate.cpp",
-        "elim.cpp",
-        "ema.cpp",
-        "extend.cpp",
-        "external.cpp",
-        "file.cpp",
-        "flags.cpp",
-        "format.cpp",
-        "gates.cpp",
-        "instantiate.cpp",
-        "internal.cpp",
-        "ipasir.cpp",
-        "limit.cpp",
-        "logging.cpp",
-        "lookahead.cpp",
-        "lucky.cpp",
-        "message.cpp",
-        "minimize.cpp",
-        "occs.cpp",
-        "options.cpp",
-        "parse.cpp",
-        "phases.cpp",
-        "probe.cpp",
-        "profile.cpp",
-        "proof.cpp",
-        "propagate.cpp",
-        "queue.cpp",
-        "random.cpp",
-        "reap.cpp",
-        "reduce.cpp",
-        "rephase.cpp",
-        "report.cpp",
-        "resources.cpp",
-        "restart.cpp",
-        "restore.cpp",
-        "score.cpp",
-        "shrink.cpp",
-        "signal.cpp",
-        "solution.cpp",
-        "solver.cpp",
-        "stats.cpp",
-        "subsume.cpp",
-        "terminal.cpp",
-        "ternary.cpp",
-        "tracer.cpp",
-        "transred.cpp",
-        "util.cpp",
-        "var.cpp",
-        "version.cpp",
-        "vivify.cpp",
-        "walk.cpp",
-        "watch.cpp",
-    ];
-    let files = files.map(|x| format!("vendor/cadical/src/{}", x));
+    let files = glob::glob("vendor/cadical/src/*.cpp")
+        .expect("Bad glob")
+        .map(|p| p.expect("Could not read file in glob"))
+        .filter(|p| {
+            let name = p.file_name().expect("Could not get file name");
+            name != "cadical.cpp" && name != "mobical.cpp"
+        })
+        .collect::<Vec<_>>();
     cc::Build::new()
         .cpp(true)
         .files(files)
@@ -106,10 +39,11 @@ fn build_static_lib() {
 fn generate_bindings_static() {
     println!("cargo:warning=Generating Cadical static bindings...");
     // Note: to generate these bindings manually, use the following command:
-    //   bindgen vendor/cadical/src/ccadical.h -o _bindings-ccadical-static.rs
+    //   bindgen vendor/cadical/src/ccadical.h -o _bindings-ccadical-static.rs --allowlist-function ccadical_.*
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindgen::builder()
         .header("vendor/cadical/src/ccadical.h")
+        .allowlist_function("ccadical_.*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Could not create bindings!")
@@ -120,11 +54,12 @@ fn generate_bindings_static() {
 fn generate_bindings_dynamic() {
     println!("cargo:warning=Generating Cadical dynamic bindings...");
     // Note: to generate these bindings manually, use the following command:
-    //   bindgen vendor/cadical/src/ccadical.h -o _bindings-ccadical-dynamic.rs --dynamic-loading ccadical
+    //   bindgen vendor/cadical/src/ccadical.h -o _bindings-ccadical-dynamic.rs --dynamic-loading ccadical  --allowlist-function ccadical_.*
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindgen::builder()
         .header("vendor/cadical/src/ccadical.h")
         .dynamic_library_name("ccadical")
+        .allowlist_function("ccadical_.*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .layout_tests(false)
         .generate()
