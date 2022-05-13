776269be8d5909fccd6a919e024425c5ac98bec9
Konstantin Chukharev <lipen00@gmail.com>
2022-05-13 23:05:00 +0300

    Add DispatchingSolver::Delegate variant

--- lib/wrappers/src/dispatch.rs
+++ lib/wrappers/src/dispatch.rs
@@ -4,6 +4,7 @@ use std::fmt::{Display, Formatter};
 use strum::IntoStaticStr;
 
 use sat_nexus_core::lit::Lit;
+use sat_nexus_core::solver::delegate::DelegateSolver;
 use sat_nexus_core::solver::{LitValue, SolveResponse, Solver};
 
 use crate::cadical::CadicalSolver;
@@ -12,11 +13,15 @@ use crate::minisat::MiniSatSolver;
 #[derive(IntoStaticStr)]
 #[strum(ascii_case_insensitive)]
 pub enum DispatchingSolver {
+    Delegate(DelegateSolver),
     MiniSat(MiniSatSolver),
     Cadical(CadicalSolver),
 }
 
 impl DispatchingSolver {
+    pub fn new_delegate(solver: impl Solver + 'static) -> Self {
+        DispatchingSolver::Delegate(DelegateSolver::new(solver))
+    }
     pub fn new_minisat() -> Self {
         DispatchingSolver::MiniSat(MiniSatSolver::new())
     }
@@ -33,12 +38,35 @@ impl DispatchingSolver {
     }
 }
 
+impl From<DelegateSolver> for DispatchingSolver {
+    fn from(inner: DelegateSolver) -> Self {
+        DispatchingSolver::Delegate(inner)
+    }
+}
+impl From<MiniSatSolver> for DispatchingSolver {
+    fn from(inner: MiniSatSolver) -> Self {
+        DispatchingSolver::MiniSat(inner)
+    }
+}
+impl From<CadicalSolver> for DispatchingSolver {
+    fn from(inner: CadicalSolver) -> Self {
+        DispatchingSolver::Cadical(inner)
+    }
+}
+
 impl Display for DispatchingSolver {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let name: &'static str = self.into();
         match self {
-            DispatchingSolver::MiniSat(inner) => write!(f, "DispatchingSolver::{}({})", name, inner),
-            DispatchingSolver::Cadical(inner) => write!(f, "DispatchingSolver::{}({})", name, inner),
+            DispatchingSolver::Delegate(inner) => {
+                write!(f, "{}::{}({})", tynm::type_name::<Self>(), name, inner)
+            }
+            DispatchingSolver::MiniSat(inner) => {
+                write!(f, "{}::{}({})", tynm::type_name::<Self>(), name, inner)
+            }
+            DispatchingSolver::Cadical(inner) => {
+                write!(f, "{}::{}({})", tynm::type_name::<Self>(), name, inner)
+            }
         }
     }
 }
@@ -46,6 +74,7 @@ impl Display for DispatchingSolver {
 impl Solver for DispatchingSolver {
     fn signature(&self) -> Cow<str> {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.signature(),
             DispatchingSolver::MiniSat(inner) => inner.signature(),
             DispatchingSolver::Cadical(inner) => inner.signature(),
         }
@@ -53,6 +82,7 @@ impl Solver for DispatchingSolver {
 
     fn reset(&mut self) {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.reset(),
             DispatchingSolver::MiniSat(inner) => inner.reset(),
             DispatchingSolver::Cadical(inner) => inner.reset(),
         }
@@ -60,6 +90,7 @@ impl Solver for DispatchingSolver {
 
     fn release(&mut self) {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.release(),
             DispatchingSolver::MiniSat(inner) => inner.release(),
             DispatchingSolver::Cadical(inner) => inner.release(),
         }
@@ -67,6 +98,7 @@ impl Solver for DispatchingSolver {
 
     fn num_vars(&self) -> usize {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.num_vars(),
             DispatchingSolver::MiniSat(inner) => inner.num_vars(),
             DispatchingSolver::Cadical(inner) => inner.num_vars(),
         }
@@ -74,6 +106,7 @@ impl Solver for DispatchingSolver {
 
     fn num_clauses(&self) -> usize {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.num_clauses(),
             DispatchingSolver::MiniSat(inner) => inner.num_clauses(),
             DispatchingSolver::Cadical(inner) => inner.num_clauses(),
         }
@@ -81,6 +114,7 @@ impl Solver for DispatchingSolver {
 
     fn new_var(&mut self) -> Lit {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.new_var(),
             DispatchingSolver::MiniSat(inner) => inner.new_var(),
             DispatchingSolver::Cadical(inner) => inner.new_var(),
         }
@@ -88,6 +122,7 @@ impl Solver for DispatchingSolver {
 
     fn assume_(&mut self, lit: Lit) {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.assume_(lit),
             DispatchingSolver::MiniSat(inner) => inner.assume_(lit),
             DispatchingSolver::Cadical(inner) => inner.assume_(lit),
         }
@@ -95,6 +130,7 @@ impl Solver for DispatchingSolver {
 
     fn add_clause_(&mut self, lits: &[Lit]) {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.add_clause_(lits),
             DispatchingSolver::MiniSat(inner) => inner.add_clause_(lits),
             DispatchingSolver::Cadical(inner) => inner.add_clause_(lits),
         }
@@ -102,6 +138,7 @@ impl Solver for DispatchingSolver {
 
     fn solve(&mut self) -> SolveResponse {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.solve(),
             DispatchingSolver::MiniSat(inner) => inner.solve(),
             DispatchingSolver::Cadical(inner) => inner.solve(),
         }
@@ -109,6 +146,7 @@ impl Solver for DispatchingSolver {
 
     fn value_(&self, lit: Lit) -> LitValue {
         match self {
+            DispatchingSolver::Delegate(inner) => inner.value_(lit),
             DispatchingSolver::MiniSat(inner) => inner.value_(lit),
             DispatchingSolver::Cadical(inner) => inner.value_(lit),
         }
