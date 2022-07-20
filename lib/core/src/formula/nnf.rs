93e6dfb02b0aa728b2fb1fc8baf8897b8116b5a1
Konstantin Chukharev <lipen00@gmail.com>
2022-07-20 15:24:19 +0300

    Replace ReificationContext with `new_var` closure

--- lib/core/src/formula/nnf.rs
+++ lib/core/src/formula/nnf.rs
@@ -32,48 +32,35 @@ impl NNF {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct CNF(Conjunction<Disjunction<i32>>);
+pub struct CNF(pub(crate) Conjunction<Disjunction<i32>>);
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct Conjunction<T>(Vec<T>);
+pub struct Conjunction<T>(pub(crate) Vec<T>);
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct Disjunction<T>(Vec<T>);
-
-#[derive(Debug)]
-pub struct ReificationContext {
-    nvars: usize,
-}
-
-impl ReificationContext {
-    pub fn new(nvars: usize) -> Self {
-        ReificationContext { nvars }
-    }
-
-    pub fn new_var(&mut self) -> usize {
-        self.nvars += 1;
-        self.nvars
-    }
-}
+pub struct Disjunction<T>(pub(crate) Vec<T>);
 
 impl NNF {
-    pub fn to_cnf(&self, ctx: &mut ReificationContext) -> CNF {
+    pub fn to_cnf<F>(&self, new_var: &mut F) -> CNF
+    where
+        F: FnMut() -> i32,
+    {
         debug!("NNF::to_cnf(self = {:?})", self);
         match self {
             NNF::Literal(lit) => CNF(Conjunction(vec![Disjunction(vec![*lit])])),
             NNF::Conjunction(args) => {
                 debug_assert_ne!(args.len(), 0, "Empty args");
-                let clauses = args.iter().map(|arg| arg.to_cnf(ctx)).flat_map(|cnf| cnf.0 .0).collect_vec();
+                let clauses = args.iter().map(|arg| arg.to_cnf(new_var)).flat_map(|cnf| cnf.0 .0).collect_vec();
                 CNF(Conjunction(clauses))
             }
             NNF::Disjunction(args) => match args.len() {
                 0 => panic!("Empty args"),
-                1 => args[0].to_cnf(ctx),
+                1 => args[0].to_cnf(new_var),
                 _ => {
                     let mut clauses = Vec::new();
                     let mut clause = Vec::new();
                     for arg in args.iter() {
-                        let (v, cls) = arg.reify(ctx);
+                        let (v, cls) = arg.reify(new_var);
                         clauses.extend(cls.into_iter().map(|cl| Disjunction(cl)));
                         clause.push(v);
                     }
@@ -84,25 +71,28 @@ impl NNF {
         }
     }
 
-    pub fn reify(&self, ctx: &mut ReificationContext) -> (i32, Vec<Vec<i32>>) {
+    pub fn reify<F>(&self, new_var: &mut F) -> (i32, Vec<Vec<i32>>)
+    where
+        F: FnMut() -> i32,
+    {
         debug!("NNF::reify(self = {:?})", self);
         match self {
             NNF::Literal(lit) => (*lit, vec![]),
             NNF::Conjunction(args) | NNF::Disjunction(args) => {
                 match args.len() {
                     0 => panic!("Empty args"),
-                    1 => args[0].reify(ctx),
+                    1 => args[0].reify(new_var),
                     _ => {
                         // Reify args
                         let mut clauses = Vec::new();
                         let mut lits = Vec::new();
                         for term in args.iter() {
-                            let (u, cls) = term.reify(ctx);
+                            let (u, cls) = term.reify(new_var);
                             clauses.extend(cls);
                             lits.push(u);
                         }
 
-                        let z = ctx.new_var() as i32;
+                        let z = new_var();
                         match self {
                             NNF::Conjunction(_) => {
                                 // Tseytin-encode 'z <=> AND(lits)'
@@ -157,56 +147,68 @@ mod tests {
 
     #[test]
     fn test_nnf_reify1() {
-        let mut ctx = ReificationContext::new(0);
-        let x1 = NNF::Literal(ctx.new_var() as i32);
-        let x2 = NNF::Literal(ctx.new_var() as i32);
-        let x3 = NNF::Literal(ctx.new_var() as i32);
-        let x4 = NNF::Literal(ctx.new_var() as i32);
+        let mut nvars = 0usize;
+        let mut new_var = || {
+            nvars += 1;
+            nvars as i32
+        };
+        let x1 = NNF::Literal(new_var());
+        let x2 = NNF::Literal(new_var());
+        let x3 = NNF::Literal(new_var());
+        let x4 = NNF::Literal(new_var());
         let expr = NNF::or([x1, NNF::and([x2, x3, x4])]);
         println!("expr = {:?}", expr);
-        let cnf = expr.to_cnf(&mut ctx);
+        let cnf = expr.to_cnf(&mut new_var);
         println!("cnf = {:?}", cnf);
+        println!("nvars = {}", nvars);
         println!("nclauses = {}", cnf.0 .0.len());
-        println!("ctx = {:?}", ctx);
         assert_eq!(cnf.0 .0.len(), 5);
     }
 
     #[test]
     fn test_nnf_reify2() {
-        let mut ctx = ReificationContext::new(0);
-        let x1 = NNF::Literal(ctx.new_var() as i32);
-        let x2 = NNF::Literal(ctx.new_var() as i32);
-        let x3 = NNF::Literal(ctx.new_var() as i32);
+        let mut nvars = 0usize;
+        let mut new_var = || {
+            nvars += 1;
+            nvars as i32
+        };
+        let x1 = NNF::Literal(new_var());
+        let x2 = NNF::Literal(new_var());
+        let x3 = NNF::Literal(new_var());
         let expr = NNF::or([
             NNF::and([x1.clone(), x2.clone(), x3.clone()]),
             NNF::and([x1.clone(), x2.clone(), x3.clone()]),
         ]);
         println!("expr = {:?}", expr);
-        let cnf = expr.to_cnf(&mut ctx);
+        let cnf = expr.to_cnf(&mut new_var);
         println!("cnf = {:?}", cnf);
+        println!("nvars = {}", nvars);
         println!("nclauses = {}", cnf.0 .0.len());
         for clause in cnf.0 .0.iter() {
             println!(". {:?}", clause);
         }
-        println!("ctx = {:?}", ctx);
         assert_eq!(cnf.0 .0.len(), 9);
     }
 
     #[test]
     fn test_nnf_reify3() {
-        let mut ctx = ReificationContext::new(0);
-        let x1 = NNF::Literal(ctx.new_var() as i32);
-        let x2 = NNF::Literal(ctx.new_var() as i32);
-        let x3 = NNF::Literal(ctx.new_var() as i32);
+        let mut nvars = 0usize;
+        let mut new_var = || {
+            nvars += 1;
+            nvars as i32
+        };
+        let x1 = NNF::Literal(new_var());
+        let x2 = NNF::Literal(new_var());
+        let x3 = NNF::Literal(new_var());
         let expr = NNF::or([NNF::or([NNF::or([NNF::and([x1, x2, x3])])])]);
         println!("expr = {:?}", expr);
-        let cnf = expr.to_cnf(&mut ctx);
+        let cnf = expr.to_cnf(&mut new_var);
         println!("cnf = {:?}", cnf);
+        println!("nvars = {}", nvars);
         println!("nclauses = {}", cnf.0 .0.len());
         for clause in cnf.0 .0.iter() {
             println!(". {:?}", clause);
         }
-        println!("ctx = {:?}", ctx);
         assert_eq!(cnf.0 .0.len(), 3);
     }
 }
