580c0c3f307dbcb40966576ef41ebe8d0baa5757
Konstantin Chukharev <lipen00@gmail.com>
2022-07-23 23:17:10 +0300

    Remove unnecessary &

--- lib/expr-parser/src/nested/parser.rs
+++ lib/expr-parser/src/nested/parser.rs
@@ -74,116 +74,116 @@ mod tests {
     #[test]
     fn test_single_var() {
         let s = "x1";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(X1));
     }
 
     #[test]
     fn test_braced_single_var() {
         let s = "(x1)";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr.map(|x| x.to_string()), Ok(X1.to_string()));
     }
     #[test]
     fn test_double_braced_single_var() {
         let s = "((x1))";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(X1));
     }
 
     #[test]
     fn test_negative_single_var() {
         let s = "~x1";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(!X1));
     }
     #[test]
     fn test_double_braced_double_negative_single_var() {
         let s = "((~~x1))";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(!!X1));
     }
 
     #[test]
     fn test_conjunction_of_two_vars() {
         let s = "x1 & x2";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(X1 & X2));
     }
     #[test]
     fn test_disjunction_of_two_vars() {
         let s = "x1 | x2";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(X1 | X2));
     }
     #[test]
     fn test_double_negated_conjunction_of_two_negated_vars() {
         let s = "~~(~x1 & ~x2)";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(!!(!X1 & !X2)));
     }
 
     #[test]
     fn test_conjunction_of_three_vars() {
         let s = "x1 & x2 & x3";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(X1 & X2 & X3));
     }
     #[test]
     fn test_disjunction_of_three_vars() {
         let s = "x1 | x2 | x3";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(X1 | X2 | X3));
     }
 
     #[test]
     fn test_mixed_expression() {
         let s = "x1 | x3 & (x2 | (~((x3))) ) & ~( x1 & ~~(x3 | (x1)) )";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(X1 | X3 & (X2 | !X3) & !(X1 & !!(X3 | X1))));
     }
 
     #[test]
     fn test_true() {
         let s = "true";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(TRUE));
     }
     #[test]
     fn test_false() {
         let s = "false";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(FALSE));
     }
     #[test]
     fn test_t() {
         let s = "T";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(TRUE));
     }
     #[test]
     fn test_f() {
         let s = "F";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(FALSE));
     }
     #[test]
     fn test_top() {
         let s = "⊤";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(TRUE));
     }
     #[test]
     fn test_bottom() {
         let s = "⊥";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(FALSE));
     }
 
     #[test]
     fn test_mixed_expression_with_constants() {
         let s = "⊤ | ~(F ) & (x2 | (~true & x1) | ⊥)";
-        let expr = parse_expr(&s);
+        let expr = parse_expr(s);
         assert_eq!(expr, Ok(TRUE | !FALSE & (X2 | !TRUE & X1 | FALSE)));
     }
 }
