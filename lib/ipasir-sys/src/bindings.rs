098a6dbd0e0956471f4777ce3ebec0bea7608f0a
Konstantin Chukharev <lipen00@gmail.com>
2023-06-23 00:41:35 +0400

    Bump libloading and bindgen

--- lib/ipasir-sys/src/bindings.rs
+++ lib/ipasir-sys/src/bindings.rs
@@ -2,7 +2,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
-#![allow(deref_nullptr)] // see https://github.com/rust-lang/rust-bindgen/issues/1651
 #![allow(clippy::style)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings-ipasir.rs"));
--- lib/minisat-sys/src/statik.rs
+++ lib/minisat-sys/src/statik.rs
@@ -3,7 +3,6 @@ pub mod bindings {
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
     #![allow(dead_code)]
-    #![allow(deref_nullptr)] // see https://github.com/rust-lang/rust-bindgen/issues/1651
     #![allow(clippy::style)]
 
     include!(concat!(env!("OUT_DIR"), "/bindings-cminisat-static.rs"));
