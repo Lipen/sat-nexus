21e5eeb46f1d94e0cd467bfa8a961e54a26bda7d
Konstantin Chukharev <lipen00@gmail.com>
2023-10-07 20:22:49 +0300

    Use imports

--- lib/ffi-utils/src/lib.rs
+++ lib/ffi-utils/src/lib.rs
@@ -1,4 +1,6 @@
-pub unsafe fn cstr2str(c_chars: *const std::ffi::c_char) -> &'static str {
-    let c_str = std::ffi::CStr::from_ptr(c_chars);
+use std::ffi::{c_char, CStr};
+
+pub unsafe fn cstr2str(c_chars: *const c_char) -> &'static str {
+    let c_str = CStr::from_ptr(c_chars);
     c_str.to_str().expect("Invalid UTF-8")
 }
