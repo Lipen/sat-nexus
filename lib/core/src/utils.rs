73a22d50e24b99399d0009ef3a62949f97ff8fdc
Konstantin Chukharev <lipen00@gmail.com>
2023-10-14 22:28:05 +0300

    Make log debug

--- lib/core/src/utils.rs
+++ lib/core/src/utils.rs
@@ -1,17 +1,17 @@
-use log::info;
+use log::debug;
 
 use crate::cnf::Cnf;
 use crate::solver::Solver;
 
 pub fn bootstrap_solver_from_cnf(solver: &mut impl Solver, cnf: &Cnf) {
     if cnf.max_var > solver.num_vars() {
-        info!("Adding {} variables...", cnf.max_var - solver.num_vars());
+        debug!("Adding {} variables...", cnf.max_var - solver.num_vars());
         for _ in solver.num_vars()..cnf.max_var {
             solver.new_var();
         }
     }
 
-    info!("Adding {} clauses...", cnf.clauses.len());
+    debug!("Adding {} clauses...", cnf.clauses.len());
     for clause in cnf.clauses.iter() {
         solver.add_clause(&clause.lits)
     }
