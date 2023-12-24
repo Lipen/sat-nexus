3dc46489764e1f823c106773c3f78fc94f772e0a
Konstantin Chukharev <lipen00@gmail.com>
2023-12-24 12:57:08 +0300

    Add search-derive binary

---
+++ examples/backdoor/src/bin/search-derive.rs
@@ -0,0 +1,232 @@
+use std::fs::File;
+use std::io::LineWriter;
+use std::io::Write;
+use std::path::PathBuf;
+use std::time::Instant;
+
+use clap::Parser;
+use log::{debug, info, trace};
+
+use backdoor::algorithm::{Algorithm, Options, DEFAULT_OPTIONS};
+use backdoor::derivation::derive_clauses;
+use backdoor::utils::{parse_comma_separated_intervals, partition_tasks};
+// use cadical_sys::statik::*;
+use simple_sat::solver::Solver;
+use simple_sat::utils::DisplaySlice;
+
+// Run this example:
+// cargo run -p backdoor --bin product-derive -- data/mult/lec_CvK_12.cnf --backdoor-size 10 --num-iters 10000 --num-runs 1000
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+struct Cli {
+    #[arg(value_name = "CNF")]
+    path_cnf: PathBuf,
+
+    /// Backdoor size.
+    #[arg(long, value_name = "INT")]
+    backdoor_size: usize,
+
+    /// Number of EA iterations.
+    #[arg(long, value_name = "INT")]
+    num_iters: usize,
+
+    /// Number of runs.
+    #[arg(long, value_name = "INT")]
+    num_runs: usize,
+
+    /// Number of conflicts.
+    #[arg(long, value_name = "INT", default_value_t = 1000)]
+    num_conflicts: usize,
+
+    /// Path to a file with derived clauses.
+    #[arg(short = 'o', long = "output", value_name = "FILE")]
+    path_output: Option<PathBuf>,
+
+    /// Path to a file with results (in CSV format).
+    #[arg(long = "results", value_name = "FILE")]
+    path_results: Option<PathBuf>,
+
+    /// Random seed.
+    #[arg(long, value_name = "INT", default_value_t = DEFAULT_OPTIONS.seed)]
+    seed: u64,
+
+    /// Do ban variables used in best backdoors on previous runs?
+    #[arg(long)]
+    ban_used: bool,
+
+    /// Comma-separated list of banned variables (1-based indices).
+    #[arg(long, value_name = "INT...")]
+    bans: Option<String>,
+
+    /// Number of stagnated iterations before re-initialization.
+    #[arg(long, value_name = "INT")]
+    stagnation_limit: Option<usize>,
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug,simple_sat::solver=info,backdoor::derivation=info"))
+        .init();
+
+    let start_time = Instant::now();
+    let args = Cli::parse();
+    info!("args = {:?}", args);
+
+    // Initialize the SAT solver:
+    let mut solver = Solver::default();
+    solver.init_from_file(&args.path_cnf);
+
+    // let solver_full = unsafe { ccadical_init() };
+    // unsafe {
+    //     for clause in solver.clauses_iter() {
+    //         for lit in clause.lits() {
+    //             ccadical_add(solver_full, lit.to_external());
+    //         }
+    //         ccadical_add(solver_full, 0)
+    //     }
+    // }
+
+    // Setup the evolutionary algorithm:
+    let options = Options {
+        seed: args.seed,
+        ban_used_variables: args.ban_used,
+        ..DEFAULT_OPTIONS
+    };
+    let mut algorithm = Algorithm::new(solver, options);
+
+    // Bans some variables:
+    if let Some(bans) = args.bans {
+        let bans = parse_comma_separated_intervals(&bans);
+        trace!("bans = {:?}", bans);
+        for i in bans {
+            assert!(i > 0);
+            algorithm.banned[i - 1] = true;
+        }
+    }
+
+    // Create and open the file with derived clauses:
+    let mut file_derived_clauses = if let Some(path) = &args.path_output {
+        let f = File::create(path)?;
+        let f = LineWriter::new(f);
+        Some(f)
+    } else {
+        None
+    };
+
+    // Create and open the file with results:
+    // let mut file_results = if let Some(path_results) = &args.path_results {
+    //     let f = File::create(path_results)?;
+    //     let f = LineWriter::new(f);
+    //     Some(f)
+    // } else {
+    //     None
+    // };
+
+    // if let Some(f) = &mut file_results {
+    //     writeln!(f, "...")?;
+    // }
+
+    for run_number in 1..=args.num_runs {
+        info!("EA run {} / {}", run_number, args.num_runs);
+
+        let result = algorithm.run(
+            args.backdoor_size,
+            args.num_iters,
+            args.stagnation_limit,
+            Some(((1u64 << args.backdoor_size) - 1) as f64 / (1u64 << args.backdoor_size) as f64),
+            100,
+        );
+        let backdoor = result.best_instance.get_variables();
+        let (hard, easy) = partition_tasks(&backdoor, &mut algorithm.solver);
+        debug!(
+            "Backdoor {} has {} hard and {} easy tasks",
+            DisplaySlice(&backdoor),
+            hard.len(),
+            easy.len()
+        );
+
+        if hard.is_empty() {
+            info!("No more cubes to solve after {} runs", run_number);
+            break;
+        }
+        if hard.len() == 1 {
+            info!("Adding {} units to the solver", hard[0].len());
+            for &lit in &hard[0] {
+                if algorithm.derived_clauses.insert(vec![lit]) {
+                    if let Some(f) = &mut file_derived_clauses {
+                        writeln!(f, "{} 0", lit)?;
+                    }
+                    algorithm.solver.add_learnt(&[lit]);
+                    // unsafe {
+                    //     ccadical_add(solver_full, lit.to_external());
+                    //     ccadical_add(solver_full, 0);
+                    // }
+                }
+            }
+            continue;
+        }
+
+        // ------------------------------------------------------------------------
+
+        info!("Deriving clauses for {} cubes...", hard.len());
+        // if hard.len() <= 30 {
+        //     for cube in hard.iter() {
+        //         debug!("cube = {}", DisplaySlice(&cube));
+        //     }
+        // }
+        let time_derive = Instant::now();
+        let derived_clauses = derive_clauses(&hard);
+        let time_derive = time_derive.elapsed();
+        info!(
+            "Derived {} clauses ({} units, {} binary, {} other) for backdoor in {:.1}s",
+            derived_clauses.len(),
+            derived_clauses.iter().filter(|c| c.len() == 1).count(),
+            derived_clauses.iter().filter(|c| c.len() == 2).count(),
+            derived_clauses.iter().filter(|c| c.len() > 2).count(),
+            time_derive.as_secs_f64()
+        );
+        // debug!("[{}]", derived_clauses.iter().map(|c| DisplaySlice(c)).join(", "));
+
+        let mut new_clauses = Vec::new();
+        for mut lemma in derived_clauses {
+            lemma.sort_by_key(|lit| lit.inner());
+            if algorithm.derived_clauses.insert(lemma.clone()) {
+                if let Some(f) = &mut file_derived_clauses {
+                    for lit in lemma.iter() {
+                        write!(f, "{} ", lit)?;
+                    }
+                    writeln!(f, "0")?;
+                }
+                algorithm.solver.add_learnt(&lemma);
+                // unsafe {
+                //     for lit in lemma.iter() {
+                //         ccadical_add(solver_full, lit.to_external());
+                //     }
+                //     ccadical_add(solver_full, 0);
+                // }
+                new_clauses.push(lemma);
+            }
+        }
+        debug!(
+            "NEW {} clauses ({} units, {} binary, {} other)",
+            new_clauses.len(),
+            new_clauses.iter().filter(|c| c.len() == 1).count(),
+            new_clauses.iter().filter(|c| c.len() == 2).count(),
+            new_clauses.iter().filter(|c| c.len() > 2).count()
+        );
+        // debug!("[{}]", new_clauses.iter().map(|c| DisplaySlice(c)).join(", "));
+    }
+
+    info!("Finished {} runs", args.num_runs);
+    info!(
+        "Total derived {} clauses ({} units, {} binary, {} other)",
+        algorithm.derived_clauses.len(),
+        algorithm.derived_clauses.iter().filter(|c| c.len() == 1).count(),
+        algorithm.derived_clauses.iter().filter(|c| c.len() == 2).count(),
+        algorithm.derived_clauses.iter().filter(|c| c.len() > 2).count()
+    );
+
+    println!("\nAll done in {:.3} s", start_time.elapsed().as_secs_f64());
+    Ok(())
+}
