e83ab4bf8f7454a10f79d89056c91e1cfc07cc94
Konstantin Chukharev <lipen00@gmail.com>
2024-01-03 12:41:12 +0300

    Add example binary with Cadical

---
+++ examples/backdoor/src/bin/cadical-solve.rs
@@ -0,0 +1,32 @@
+use std::time::Instant;
+
+use cadical::statik::*;
+use simple_sat::utils::parse_dimacs;
+
+// Run:
+// cargo run --release -p backdoor --bin cadical-solve -- data/my.cnf
+
+fn main() {
+    let start_time = Instant::now();
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+
+    let solver = Cadical::new();
+    println!("signature = {}", solver.signature());
+
+    println!("Adding clauses from '{}'...", path);
+    let mut num_clauses = 0;
+    for clause in parse_dimacs(path) {
+        num_clauses += 1;
+        solver.add_clause(clause.into_iter().map(|lit| lit.to_external()));
+    }
+    println!("num_clauses = {}", num_clauses);
+    println!("solver.vars() = {}", solver.vars());
+
+    println!("Solving...");
+    let res = solver.solve();
+    println!("res = {:?}", res);
+
+    let total_time = start_time.elapsed();
+    println!("\nAll done in {:.3} s", total_time.as_secs_f64());
+}
