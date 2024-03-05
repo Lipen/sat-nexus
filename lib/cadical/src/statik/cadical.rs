66847b6e035b0f844e591c73e5a8b0cba4a39d35
Konstantin Chukharev <lipen00@gmail.com>
2024-03-05 13:05:11 +0300

    Remove Vec-based get_all_clauses, minimize unsafe scope

--- lib/cadical/src/statik/cadical.rs
+++ lib/cadical/src/statik/cadical.rs
@@ -349,29 +349,12 @@ impl Cadical {
         }
     }
 
-    pub fn get_all_clauses(&self) -> Vec<Vec<i32>> {
-        unsafe {
-            let all_clauses_length = ccadical_build_all_clauses(self.ptr);
-            let mut all_clauses = Vec::with_capacity(all_clauses_length);
-            for i in 0..all_clauses_length {
-                let clause_length = ccadical_get_clause_length(self.ptr, i);
-                let mut clause = Vec::with_capacity(clause_length);
-                ccadical_get_clause(self.ptr, i, clause.as_mut_ptr());
-                clause.set_len(clause_length);
-                all_clauses.push(clause);
-            }
-            all_clauses
-        }
-    }
-
     pub fn all_clauses_iter(&self) -> AllClausesIter {
-        unsafe {
-            let length = ccadical_build_all_clauses(self.ptr);
-            AllClausesIter {
-                ptr: self.ptr,
-                index: 0,
-                length,
-            }
+        let length = unsafe { ccadical_build_all_clauses(self.ptr) };
+        AllClausesIter {
+            ptr: self.ptr,
+            index: 0,
+            length,
         }
     }
 }
