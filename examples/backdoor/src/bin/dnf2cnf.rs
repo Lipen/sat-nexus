63816d1ee4e34ed2d62a7809ef19860b69e3a120
Konstantin Chukharev <lipen00@gmail.com>
2024-08-09 11:36:34 +0300

    Update dnf2cnf

--- examples/backdoor/src/bin/dnf2cnf.rs
+++ examples/backdoor/src/bin/dnf2cnf.rs
@@ -1,17 +1,16 @@
 use std::fs::File;
-use std::io::Write;
-use std::io::{BufRead, BufReader, BufWriter};
+use std::io::{BufRead, BufReader, LineWriter, Write};
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 
-use backdoor::utils::bdd_cnf_encode;
+use backdoor::utils::{bdd_cnf_encode, bdd_tseytin_encode_ite, clause_to_external};
 
 use bdd_rs::bdd::Bdd;
 use simple_sat::lit::Lit;
 
 use clap::{Parser, ValueEnum};
 use itertools::Itertools;
-use log::info;
+use log::{debug, info};
 
 #[derive(Parser, Debug)]
 #[command(author, version)]
@@ -24,17 +23,21 @@ struct Cli {
     #[arg(long = "clauses", value_name = "FILE")]
     path_clauses: Option<PathBuf>,
 
-    /// Encoding to use for converting DNF to CNF.
-    #[arg(long, default_value = "tseytin")]
-    encoding: Encoding,
-
-    /// Number of variables in the original CNF.
-    #[arg(long)]
-    num_vars: Option<u64>,
-
     /// Path to a file with intermediate results.
     #[arg(long = "results", value_name = "FILE")]
     path_results: Option<PathBuf>,
+
+    /// Encoding to use for converting DNF to CNF.
+    #[arg(long, value_name = "ENCODING", value_enum)]
+    encoding: Encoding,
+
+    /// Number of variables in the original CNF.
+    #[arg(long, value_name = "INT")]
+    num_vars: Option<u64>,
+
+    /// Number of bits to use for storage in BDD.
+    #[arg(long, value_name = "INT")]
+    storage_bits: Option<usize>,
 }
 
 #[derive(ValueEnum, Debug, Copy, Clone)]
@@ -51,81 +54,100 @@ fn main() -> color_eyre::Result<()> {
 
     let start_time = Instant::now();
     let args = Cli::parse();
-    info!("args = {:?}", args);
+    debug!("args = {:?}", args);
 
     info!("Reading cubes from '{}'...", args.path_cubes.display());
     let cubes = read_cubes(args.path_cubes);
     info!("Total {} cubes", cubes.len());
 
-    // let mut clauses = Vec::new();
+    let encoded_clauses: Vec<Vec<Lit>>;
 
-    // if false {
-    //     let mut hard2_clauses = Vec::new();
-    //     for cube in hard2_cubes.iter() {
-    //         num_vars += 1;
-    //         let aux = Lit::from_external(num_vars);
-    //         hard2_clauses.extend(encode_clause_tseytin(aux, cube));
-    //     }
-    //     println!(
-    //         "Tseytin-encoded {} hard cubes in the second part into {} clauses with {} aux vars",
-    //         hard2_cubes.len(),
-    //         hard2_clauses.len(),
-    //         num_vars - max_var
-    //     );
-    //     clauses.extend(hard2_clauses);
-    // }
-    // if false {
-    //     let path_output = "data/2024-08-07/out_orig_with_cubes2_tseytin.cnf";
-    //     println!(
-    //         "Writing CNF with {} vars and {} clauses to '{}'",
-    //         num_vars,
-    //         clauses.len(),
-    //         path_output
-    //     );
-    //     let file = File::create(path_output)?;
-    //     let mut writer = BufWriter::new(&file);
-    //     writeln!(writer, "p cnf {} {}", num_vars, clauses.len())?;
-    //     for clause in clauses.iter() {
-    //         for &lit in clause.iter() {
-    //             write!(writer, "{} ", lit)?;
-    //         }
-    //         writeln!(writer, "0")?;
-    //     }
-    // }
-    //
-    // if true {
-    //     let bdd = Bdd::new(24);
-    //     let mut hard2_cubes_bdds = Vec::new();
-    //     for cube in hard2_cubes.iter() {
-    //         let c = bdd.cube(cube.iter().map(|lit| lit.to_external()));
-    //         hard2_cubes_bdds.push(c);
-    //     }
-    //     let f = bdd.apply_or_many(hard2_cubes_bdds.iter().copied());
-    //     println!("f = {} of size {}", f, bdd.size(f));
-    //     let hard2_clauses = bdd_cnf_encode(&bdd, f);
-    //     println!(
-    //         "BDD-encoded {} hard cubes in the second part into {} clauses",
-    //         hard2_cubes.len(),
-    //         hard2_clauses.len()
-    //     );
-    //     clauses.extend(hard2_clauses);
-    // }
+    match args.encoding {
+        Encoding::Naive => todo!(),
 
-    // let path_output = "data/2024-08-07/out.cnf";
-    // if true {
-    //     println!(
-    //         "Writing CNF with {} vars and {} clauses to '{}'",
-    //         num_vars,
-    //         clauses.len(),
-    //         path_output
-    //     );
-    //     let file = File::create(path_output)?;
-    //     let mut writer = BufWriter::new(&file);
-    //     writeln!(writer, "p cnf {} {}", num_vars, clauses.len())?;
-    //     for clause in clauses.iter() {
-    //         write_clause(&mut writer, clause)?;
-    //     }
-    // }
+        Encoding::Tseytin => {
+            info!("Using Tseytin method");
+
+            let num_vars_original = args
+                .num_vars
+                .expect("Number of variables in the original CNF must be provided (via `--num-vars <INT>`) when using Tseytin encoding");
+            let mut num_vars = num_vars_original;
+
+            let mut clauses = Vec::new();
+            info!("Building CNF via Tseytin-encoding...");
+            for cube in cubes.iter() {
+                num_vars += 1;
+                let aux = Lit::from_external(num_vars as i32);
+                clauses.extend(encode_clause_tseytin(aux, cube));
+            }
+            info!("Total {} clauses and {} new vars", clauses.len(), num_vars - num_vars_original);
+            encoded_clauses = clauses;
+        }
+
+        Encoding::BddPaths => {
+            info!("Using BDD-paths method");
+
+            let bdd = Bdd::new(args.storage_bits.unwrap_or(20));
+
+            let mut cubes_bdd = Vec::new();
+            info!("Building BDDs for {} cubes...", cubes.len());
+            for cube in cubes.iter() {
+                let c = bdd.cube(clause_to_external(cube));
+                cubes_bdd.push(c);
+            }
+            debug!("bdd = {:?}", bdd);
+
+            info!("Merging BDDs for {} cubes via Apply(OR)...", cubes_bdd.len());
+            let f = bdd.apply_or_many(cubes_bdd);
+            info!("f = {} of size {}", f, bdd.size(f));
+            debug!("bdd = {:?}", bdd);
+
+            info!("Building CNF via enumerating all paths to 0...");
+            let clauses = bdd_cnf_encode(&bdd, f);
+            info!("Total {} clauses", clauses.len());
+            encoded_clauses = clauses;
+        }
+
+        Encoding::BddTseytin => {
+            info!("Using BDD-Tseytin method");
+
+            let num_vars_original = args.num_vars.expect(
+                "Number of variables in the original CNF must be provided (via `--num-vars <INT>`) when using BDD-Tseytin encoding",
+            );
+
+            let bdd = Bdd::new(args.storage_bits.unwrap_or(20));
+
+            let mut cubes_bdd = Vec::new();
+            info!("Building BDDs for {} cubes...", cubes.len());
+            for cube in cubes.iter() {
+                let c = bdd.cube(clause_to_external(cube));
+                cubes_bdd.push(c);
+            }
+            debug!("bdd = {:?}", bdd);
+
+            info!("Merging BDDs for {} cubes via Apply(OR)...", cubes_bdd.len());
+            let f = bdd.apply_or_many(cubes_bdd);
+            info!("f = {} of size {}", f, bdd.size(f));
+            debug!("bdd = {:?}", bdd);
+
+            info!("Building CNF via Tseytin-encoding the BDD...");
+            let (clauses, extra_vars) = bdd_tseytin_encode_ite(&bdd, f, num_vars_original);
+            info!("Total {} clauses and {} new vars", clauses.len(), extra_vars.len());
+            encoded_clauses = clauses;
+        }
+    }
+
+    if let Some(path_clauses) = args.path_clauses {
+        info!("Writing {} clauses into '{}'...", encoded_clauses.len(), path_clauses.display());
+        let file = File::create(path_clauses)?;
+        let mut file = LineWriter::new(file);
+        for clause in encoded_clauses.iter() {
+            for lit in clause.iter() {
+                write!(&mut file, "{} ", lit)?;
+            }
+            writeln!(&mut file, "0")?;
+        }
+    }
 
     let total_time = start_time.elapsed();
     println!("\nAll done in {:.3} s", total_time.as_secs_f64());
@@ -156,9 +178,14 @@ fn read_cubes(path: impl AsRef<Path>) -> Vec<Vec<Lit>> {
 
 fn encode_clause_tseytin(aux: Lit, clause: &Vec<Lit>) -> Vec<Vec<Lit>> {
     let mut clauses = Vec::with_capacity(clause.len() + 1);
+
+    // Binary clauses (-lit, aux)
     for &lit in clause {
         clauses.push(vec![-lit, aux]);
     }
+
+    // Long clause (lit1, lit2, ..., litN, -aux)
     clauses.push(clause.iter().copied().chain([-aux]).collect());
+
     clauses
 }
