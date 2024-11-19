f5a8b626b90376ea47b3666f9a8fbf3a73755cd8
Konstantin Chukharev <lipen00@gmail.com>
2024-11-20 00:21:52 +0300

    Add Model::new

--- lib/core/src/model.rs
+++ lib/core/src/model.rs
@@ -7,6 +7,12 @@ pub struct Model {
 }
 
 impl Model {
+    pub fn new(data: Vec<bool>) -> Self {
+        Self { data }
+    }
+}
+
+impl Model {
     fn get(&self, lit: Lit) -> bool {
         let value = self.data[lit.var() as usize];
         let is_pos = lit.get() < 0;
