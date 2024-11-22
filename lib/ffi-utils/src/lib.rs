a4046fd11f337b23b65d925cf0a2875891ff5857
Konstantin Chukharev <lipen00@gmail.com>
2024-11-23 01:28:01 +0300

    Add lifetime parameter to cstr2str

--- lib/ffi-utils/src/lib.rs
+++ lib/ffi-utils/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ffi::{c_char, CStr};
 
-pub unsafe fn cstr2str(c_chars: *const c_char) -> &'static str {
+pub unsafe fn cstr2str<'a>(c_chars: *const c_char) -> &'a str {
     let c_str = CStr::from_ptr(c_chars);
     c_str.to_str().expect("Invalid UTF-8")
 }
