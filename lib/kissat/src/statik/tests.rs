90f5f5ee07a7f2eedba6926f5cbf93841f9201a8
Konstantin Chukharev <lipen00@gmail.com>
2024-11-23 22:53:39 +0300

    Add tests for static Kissat

--- lib/kissat/src/statik/tests.rs
+++ lib/kissat/src/statik/tests.rs
@@ -1,1 +1,27 @@
 use super::*;
+
+#[test]
+fn test_kissat_solver() -> color_eyre::Result<()> {
+    let solver = Kissat::new();
+    assert!(solver.signature().contains("kissat"));
+
+    // Adding [(1 or 2) and (3 or 4) and not(1 and 2) and not(3 and 4)]
+    solver.add_clause([1, 2]);
+    solver.add_clause([3, 4]);
+    solver.add_clause(vec![-1, -2]);
+    solver.add_clause(vec![-3, -4]);
+
+    // Problem is satisfiable
+    let response = solver.solve();
+    assert_eq!(response, SolveResponse::Sat);
+
+    let val1 = solver.value(1);
+    let val2 = solver.value(2);
+    let val3 = solver.value(3);
+    let val4 = solver.value(4);
+    println!("values: {:?}", vec![val1, val2, val3, val4]);
+    assert!(bool::from(val1) ^ bool::from(val2));
+    assert!(bool::from(val3) ^ bool::from(val4));
+
+    Ok(())
+}
