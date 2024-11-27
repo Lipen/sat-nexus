4a0dfe2256eef1701c472b6069e4a1a121acf289
Konstantin Chukharev <lipen00@gmail.com>
2024-11-28 02:00:13 +0300

    Cleanup

--- lib/kissat-sys/build.rs
+++ lib/kissat-sys/build.rs
@@ -17,7 +17,7 @@ fn generate_bindings_dynamic() {
     build_script::cargo_rerun_if_changed("wrapper.h");
 
     // Note: to generate these bindings manually, use the following command:
-    //   bindgen wrapper.h -o _bindings-kissat-dynamic.rs --dynamic-loading kissat_ffi --dynamic-link-require-all --no-layout-tests --allowlist-function "kissat_.*" --blocklist-item "kissat_changes" --blocklist-item "kissat_changed" --blocklist-item "kissat_options_print_value" --clang-arg "-DCOMPACT" --clang-arg "-DNDEBUG" --clang-arg "-DNOPTIONS" --clang-arg "-DNPROOFS" --clang-arg "-DQUIET"
+    //   bindgen wrapper.h -o _bindings-kissat-dynamic.rs --dynamic-loading kissat_ffi --dynamic-link-require-all --no-layout-tests --allowlist-function "kissat_.*" --blocklist-item "kissat_changes" --blocklist-item "kissat_changed" --blocklist-item "kissat_options_print_value" --clang-arg "-DCOMPACT" --clang-arg "-DNDEBUG" --clang-arg "-DNPROOFS" --clang-arg "-DQUIET"
     let bindings = bindgen::builder()
         .header("wrapper.h")
         .dynamic_library_name("kissat_ffi")
@@ -28,7 +28,6 @@ fn generate_bindings_dynamic() {
         .blocklist_item("kissat_options_print_value")
         .clang_arg("-DCOMPACT")
         .clang_arg("-DNDEBUG")
-        // .clang_arg("-DNOPTIONS")
         .clang_arg("-DNPROOFS")
         .clang_arg("-DQUIET")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
@@ -70,7 +69,6 @@ fn build_static_lib() {
         .warnings(false)
         .define("COMPACT", None)
         .define("NDEBUG", None)
-        // .define("NOPTIONS", None)
         .define("NPROOFS", None)
         .define("QUIET", None);
 
