e5e740d43395a0abf21e68144a2362f0f78cf791
Konstantin Chukharev <lipen00@gmail.com>
2025-03-06 13:15:12 +0300

    Use c_str!

--- examples/pyeda/src/lib.rs
+++ examples/pyeda/src/lib.rs
@@ -1,3 +1,4 @@
+use pyo3::ffi::c_str;
 use pyo3::prelude::*;
 
 pub fn backdoor_to_clauses(cubes: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
@@ -8,8 +9,8 @@ pub fn backdoor_to_clauses(cubes: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         // let pyeda_version: String = pyeda.getattr("__version__")?.extract()?;
         // println!("pyeda.__version__ = {:?}", pyeda_version);
 
-        let py_common_code = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/python_code/common.py"));
-        let py_common = PyModule::from_code(py, py_common_code, "common.py", "common")?;
+        let py_common_code = c_str!(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/python_code/common.py")));
+        let py_common = PyModule::from_code(py, py_common_code, c_str!("common.py"), c_str!("common"))?;
         let f: Py<PyAny> = py_common.getattr("backdoor_to_clauses")?.into();
         // println!("f = {}", f);
 
